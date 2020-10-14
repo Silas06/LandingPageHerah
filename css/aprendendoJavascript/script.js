535b92a2762fc3267fc7beceda66ed21b379f2cf
Silas Oliveira <silas.spo@hotmail.com>
2020-10-14 20:14:22 -0300

    adicionandoContainer

---
+++ css/aprendendoJavascript/script.js
@@ -0,0 +1,11 @@
+//alert("Hola Mundo!");
+
+let pessoa = {
+    nome: 'silas',
+    idade: 29,
+    peso: 80,
+    altura: 1.70,
+    casado: true,
+    };
+
+    console.log(`Bem vindo ${pessoa.nome}   ` );
